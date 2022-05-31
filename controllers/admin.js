52b801d337a25fcdcfbd673bd4ba2f343495d059
Tiago Rodrigues <ttiagorrodrigues@gmail.com>
2022-05-30 23:56:46 -0300

    Initialize Project

---
+++ controllers/admin.js
@@ -0,0 +1,45 @@
+const Product = require('../models/product');
+
+exports.getAddProduct = (req, res, next) => {
+	res.render('admin/edit-product', {
+		pageTitle: 'Add Product',
+		path: '/admin/add-product',
+		edit: false,
+		formsCSS: true,
+		productCSS: true,
+		activeAddProduct: true,
+	});
+};
+
+exports.postAddProduct = (req, res, next) => {
+	const title = req.body.title;
+	const imageUrl = req.body.imageUrl;
+	const price = req.body.price;
+	const description = req.body.description;
+	const product = new Product(title, imageUrl, description, price);
+	product.save();
+	res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+	Product.fetchAll((products) => {
+		res.render('admin/products', {
+			prods: products,
+			pageTitle: 'Admin Products',
+			path: '/admin/products',
+		});
+	});
+};
+
+exports.getEditProduct = (req, res, next) => {
+	let edit = req.query.edit;
+	let productID = req.query.productID;
+	Product.fetchById(productID, (product) => {
+		res.render('admin/edit-product', {
+			pageTitle: 'Edit Product',
+			path: '/admin/edit-product',
+			edit: true,
+			product: product,
+		});
+	});
+};
