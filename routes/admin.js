7c6a9632ac9b303f4b53ac86ee1356551860760a
Tiago Rodrigues <3497845+tiagorodriguesdev@users.noreply.github.com>
2022-06-02 02:34:16 -0300

    Add postEditProduct route

--- routes/admin.js
+++ routes/admin.js
@@ -6,15 +6,14 @@ const adminController = require('../controllers/admin');
 
 const router = express.Router();
 
-// /admin/add-product => GET
 router.get('/add-product', adminController.getAddProduct);
 
-// /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
-router.get('/edit-product', adminController.getEditProduct);
+router.get('/edit-product/:productId', adminController.getEditProduct);
 
-// /admin/products => GET
+router.post('/edit-product', adminController.postEditProduct);
+
 router.get('/products', adminController.getProducts);
 
 module.exports = router;
