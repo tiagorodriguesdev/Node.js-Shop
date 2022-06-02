20a08558a28e82505ffb9eb3d327c0ce350ff6cd
Tiago Rodrigues <3497845+tiagorodriguesdev@users.noreply.github.com>
2022-06-02 02:32:34 -0300

    Add postEditProduct controller and minor fixes

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
 	const imageUrl = req.body.imageUrl;
 	const price = req.body.price;
 	const description = req.body.description;
-	const product = new Product(title, imageUrl, description, price);
+	const product = new Product(null, title, imageUrl, description, price);
 	product.save();
 	res.redirect('/');
 };
@@ -30,8 +30,8 @@ exports.getProducts = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
 	let edit = req.query.edit;
-	let productID = req.query.productID;
-	Product.fetchById(productID, (product) => {
+	let productId = req.params.productId;
+	Product.fetchById(productId, (product) => {
 		res.render('admin/edit-product', {
 			pageTitle: 'Edit Product',
 			path: '/admin/edit-product',
@@ -39,4 +39,15 @@ exports.getEditProduct = (req, res, next) => {
 			product: product,
 		});
 	});
+};
+
+exports.postEditProduct = (req, res, next) => {
+	const id = req.body.productID;
+	const title = req.body.title;
+	const imageUrl = req.body.imageUrl;
+	const price = req.body.price;
+	const description = req.body.description;
+	const updatedProduct = new Product(id, title, imageUrl, description, price);
+	updatedProduct.save();
+	res.redirect('/admin/products');
 };
