755c27abb494d36a0d3b7913de0c5579b718bb84
Tiago Rodrigues <3497845+tiagorodriguesdev@users.noreply.github.com>
2022-06-06 10:14:53 -0300

    Remove product from cart after deletion

--- models/product.js
+++ models/product.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const Cart = require('./cart');
 
 const p = path.join(path.dirname(require.main.filename), 'data', 'products.json');
 
@@ -43,10 +44,11 @@ module.exports = class Product {
 
 	static deleteById(id) {
 		getProductsFromFile((products) => {
+			const product = products.find((prod) => prod.id === id);
 			const updatedProducts = products.filter((prod) => prod.id !== id);
 			fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
 				if (!err) {
-					//remover do carrinho
+					Cart.deleteProduct(id, product.price);
 				}
 			});
 		});
