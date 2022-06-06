60e6e8e015f5f620f590bd00298b7a2e11a0dd68
Tiago Rodrigues <3497845+tiagorodriguesdev@users.noreply.github.com>
2022-06-06 09:14:27 -0300

    Fixes cart models

--- models/cart.js
+++ models/cart.js
@@ -33,18 +33,19 @@ module.exports = class Cart {
 			if (!err) {
 				cart = JSON.parse(fileContent);
 			}
-			const existingProduct = cart.products.find((prod) => prod.id === id);
+			const existingProductIndex = cart.products.findIndex((prod) => prod.id === id);
+			const existingProduct = cart.products[existingProductIndex];
 			let updatedProduct;
 			if (existingProduct) {
 				updatedProduct = { ...existingProduct };
 				updatedProduct.qty = updatedProduct.qty + 1;
-				// cart.products = {...cart.products};
-				// cart.products[existingProductIndex] = updatedProduct;
+				cart.products = [...cart.products];
+				cart.products[existingProductIndex] = updatedProduct;
 			} else {
 				updatedProduct = { id: id, qty: 1 };
+				cart.products = [...cart.products, updatedProduct];
 			}
 			cart.totalPrice = cart.totalPrice + +productPrice;
-			cart.products = [...cart.products, updatedProduct];
 
 			fs.writeFile(cartData, JSON.stringify(cart), (err) => {
 				console.log(err);
